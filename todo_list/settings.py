480d5cb569cb4b4f9357f4d2c705fac7a4e40f62
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-05-22 14:57:29 -0300

    Cadastro de usuários

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-LOGIN_REDIRECT_URL = 'tasks'
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'tasks'
+
+LOGIN_URL = 'login'
